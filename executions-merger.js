7f6f8fd33ab3286375b214cc3fec160e14148143
Juan Sebastián Gaitán Villamizar <juans.gaitan@gmail.com>
2017-11-16 14:55:17 +0100

    Added inSequence parameter to getAllExecutionResults

--- executions-merger.js
+++ executions-merger.js
@@ -14,7 +14,6 @@ async function getExecution(executionId, limit, offset) {
 async function getExecutionResults(execId) {
   const { items } = await getExecution(execId);
   log(`Execution Results for ${execId}: ${items.length}`);
-
   return items.reduce((acc, { pageFunctionResult: result }) => {
     if (result) {
       acc.push(result);
@@ -23,21 +22,38 @@ async function getExecutionResults(execId) {
   }, []);
 }
 
-async function getAllExecutionResults(execIds) {
+function getInSequence(items, asyncFunction) {
+  return items.reduce((previous, item) => (
+    previous.then(accumulator => (
+      asyncFunction(item).then(result => [...accumulator, ...result])
+    ))
+  ), Promise.resolve([]));
+}
+
+async function getAllExecutionResults(execIds, inSequence = false) {
+  if (inSequence) {
+    log('Getting Execution Results in Sequence...');
+    return getInSequence(execIds, getExecutionResults);
+  }
+  log('Getting Execution Results in Parallel...');
   const execPromises = execIds.map(getExecutionResults);
   const allResults = await Promise.all(execPromises);
   const flattenedResults = allResults.reduce((prev, cur) => [...prev, ...cur]);
   log(`All Execution Results: ${flattenedResults.length}`);
+
   return flattenedResults;
 }
 
 Apify.main(async () => {
-  const { executionIds } = await Apify.getValue('INPUT');
+  const input = await Apify.getValue('INPUT');
+  log(input);
+  const { executionIds, inSequence } = input;
+
 
   if (!executionIds) {
     throw new Error('ERROR: Missing "executionIds" attribute in INPUT');
   }
 
-  const results = await getAllExecutionResults(executionIds);
+  const results = await getAllExecutionResults(executionIds, inSequence);
   await Apify.setValue('OUTPUT', results);
 });
