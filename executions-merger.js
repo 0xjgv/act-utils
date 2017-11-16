ac52ae04da0e520be49601b6cdba4d1a0d362535
Juan Sebastián Gaitán Villamizar <juans.gaitan@gmail.com>
2017-11-16 15:20:21 +0100

    Added Error handling in getInSequence

--- executions-merger.js
+++ executions-merger.js
@@ -1,9 +1,7 @@
 const Apify = require('apify');
 
-const { log } = console;
-
 async function getExecution(executionId, limit, offset) {
-  log(`Getting Execution Result: ${executionId}`);
+  console.log(`Getting Execution Result: ${executionId}`);
   return Apify.client.crawlers.getExecutionResults({
     executionId,
     limit,
@@ -13,7 +11,7 @@ async function getExecution(executionId, limit, offset) {
 
 async function getExecutionResults(execId) {
   const { items } = await getExecution(execId);
-  log(`Execution Results for ${execId}: ${items.length}`);
+  console.log(`Execution Results for ${execId}: ${items.length}`);
   return items.reduce((acc, { pageFunctionResult: result }) => {
     if (result) {
       acc.push(result);
@@ -32,11 +30,15 @@ function getInSequence(items, asyncFunction) {
 
 async function getAllExecutionResults(execIds, inSequence = false) {
   if (inSequence) {
-    log('Getting Execution Results in Sequence...');
-    return getInSequence(execIds, getExecutionResults);
+    console.log('Getting Execution Results in Sequence...');
+    try {
+      return getInSequence(execIds, getExecutionResults);
+    } catch (error) {
+      console.log(`Error getAllExecutionResults: ${error}`);
+    }
   }
 
-  log('Getting Execution Results in Parallel...');
+  console.log('Getting Execution Results in Parallel...');
   const execPromises = execIds.map(getExecutionResults);
   const allResults = await Promise.all(execPromises);
   return allResults.reduce((prev, cur) => [...prev, ...cur]);
@@ -44,13 +46,11 @@ async function getAllExecutionResults(execIds, inSequence = false) {
 
 Apify.main(async () => {
   const { executionIds, inSequence } = await Apify.getValue('INPUT');
-
   if (!executionIds) {
     throw new Error('ERROR: Missing "executionIds" attribute in INPUT');
   }
 
   const results = await getAllExecutionResults(executionIds, inSequence);
-  log(`All Execution Results: ${results.length}`);
-
+  console.log(`All Execution Results: ${results.length}`);
   await Apify.setValue('OUTPUT', results);
 });
