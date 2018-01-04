f61f19e65cfa99cbccf2a29fefbe3a09e2cb1e8b
Juan Sebastián Gaitán Villamizar <juans.gaitan@gmail.com>
2018-01-04 10:40:46 +0100

    Update link-extractor with STATE

--- link-extractor.js
+++ link-extractor.js
@@ -1,8 +1,10 @@
+const _ = require('lodash');
 const { URL } = require('url');
 const Apify = require('apify');
 const { typeCheck } = require('type-check');
 
 const { log, dir } = console;
+const pretty = object => JSON.stringify(object, null, 2);
 
 const INPUT_TYPE = `{
   baseUrl: String,
@@ -34,7 +36,7 @@ async function extractUrls(browser, username, url, pageFunc, cssSelector) {
     await page.waitForSelector(cssSelector);
 
     const postsUrls = await page.evaluate((fn) => {
-      const func = new Function(fn);
+      const func = new Function(fn); // eslint-disable-line
       return func();
     }, pageFunc);
 
@@ -55,7 +57,7 @@ Apify.main(async () => {
   if (typeof input === 'string') {
     input = JSON.parse(input);
   }
-  log(input);
+
   if (!typeCheck(INPUT_TYPE, input)) {
     log('Expected input:');
     log(INPUT_TYPE);
@@ -63,13 +65,44 @@ Apify.main(async () => {
     dir(input);
     throw new Error('Received invalid input');
   }
+
   const {
     baseUrl,
     usernames,
     pageFunction,
     waitForCssSelector,
   } = input;
-  log(baseUrl, usernames);
+  usernames.sort();
+  log('Target URL:', baseUrl);
+  log('Usernames:', usernames);
+
+  // Check previous run implementation
+  const { keyValueStores } = Apify.client;
+  const { id: storeId } = await keyValueStores.getOrCreateStore({
+    storeName: 'Link-Extractor',
+  });
+  Apify.client.setOptions({ storeId });
+
+  const record = await keyValueStores.getRecord({ key: 'STATE' });
+  const storeRecord = record && record.body ? record.body : {};
+  const previousState = typeof storeRecord === 'string' ?
+    JSON.parse(storeRecord) : storeRecord;
+  log('Previous STATE:', pretty(previousState));
+
+  const previousUsernames = (previousState.usernames || []).sort();
+  const isSameCall = _.isEqual(previousUsernames, usernames);
+  log('Is the same call?', isSameCall);
+
+  const now = Date.now();
+  const elapsedTime = new Date(now - (previousState.timestamp || 0));
+  log(`Time elapsed since last call: ${elapsedTime.getMinutes()} minutes`);
+
+  if (elapsedTime.getMinutes() < 15 && isSameCall) {
+    log(previousState.urls);
+    await Apify.setValue('OUTPUT', previousState.urls);
+    log('Done.');
+    return previousState;
+  }
 
   log('Openning browser...');
   const browser = await Apify.launchPuppeteer();
@@ -81,9 +114,18 @@ Apify.main(async () => {
     return extractUrls(browser, username, href, pageFunction, waitForCssSelector);
   });
   const urls = await Promise.all(allExtractedUrls);
+
+  const timestamp = Date.now();
+  await keyValueStores.putRecord({
+    key: 'STATE',
+    body: pretty(Object.assign({}, { urls, timestamp, usernames })),
+  });
+
   await Apify.setValue('OUTPUT', urls);
   log(JSON.stringify(urls, null, 2));
 
   log('Closing browser.');
+  log('Done.');
   await browser.close();
+  return previousState;
 });
